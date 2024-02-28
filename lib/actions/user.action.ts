2873d274f0cc4fe914c4b0b5f42d2c731b1bec28
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-02-28 16:07:51 +0000

    Adding Webhhoks (form clerk) user and question action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -12,6 +12,8 @@ import type {
     ToggleSaveQuestionParams,
     UpdateUserParams,
   } from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
   
 
 export async function getUserById(params: { userId: string }) {
@@ -30,4 +32,63 @@ export async function getUserById(params: { userId: string }) {
         console.log(error, " -->in user.action.ts");
         
     }
+}
+
+export async function createUser(userData :CreateUserParams) {
+  try {
+    connectToDatabase()
+      
+    const newUser = await User.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error
+  }
+  
+}
+export async function updateUser(userData :UpdateUserParams) {
+  try {
+    connectToDatabase();
+      
+    const {clerkId , updateData, path} = userData;
+    await User.findOneAndUpdate({clerkId: clerkId}, updateData, {new: true});
+
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error
+  }
+  
+}
+export async function deleteUser(params :DeleteUserParams) {
+  try {
+    connectToDatabase();
+      const {clerkId} = params;
+
+      const user = await User.findOneAndDelete({clerkId: clerkId});
+
+      if(!user){
+        throw new Error('User not found')
+      }
+
+      const userQuestionIds = await Question.find({author: user._id}).distinct('id')
+
+      await Question.deleteMany({author: user._id});
+
+      //Todo: delete user answers, comments, and saved questions
+      const deletedUser = await User.findOneAndDelete({clerkId: clerkId});
+
+      return deletedUser;
+
+
+
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error
+  }
+  
 }
