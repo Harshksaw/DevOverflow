f9ebb787d5bbc85fd756c18dbb88fd134640cb8e
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-04 22:07:30 +0530

    Question page

--- lib/validation.ts
+++ lib/validation.ts
@@ -7,3 +7,7 @@ export const QuestionSchema = z.object({
     tags: z.array(z.string().min(1).max(15)).min(1).max(3),
 
   })
+
+  export const AnswerSchema= z.object({
+    answer : z.string().min(100).max(1000),
+  })
