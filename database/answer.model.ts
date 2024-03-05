4cb6edd2987f438300ac58a99acfc0395bd2a5aa
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-05 15:26:26 +0000

    Add Answer model and create Answer action

---
+++ database/answer.model.ts
@@ -0,0 +1,30 @@
+import { Schema, models, model, Document } from "mongoose";
+
+export interface IAnswer extends Document {
+    author: Schema.Types.ObjectId;
+    Question: Schema.Type.ObjectId;
+    content:string;
+    upvotes:Schema.Types.ObjectId[];
+    downvotes: Schema.Types.ObjectId[];
+    createdAt: Date;
+
+
+  
+}
+
+const AnswerSchema = new Schema({
+    author: {type: Schema.Types.ObjectId, ref : 'User', required: true},
+    question : {
+        type: Schema.Types.ObjectId , ref: 'Question', required: true
+    },
+
+    content: {type: String , required: true},
+    upvotes: [{type: Schema.Type.ObjectId,ref: 'User'}],
+    downvotes: [{type: Schema.Type.ObjectId,ref: 'User'}],
+    createdAt: {type: Date, default: Date.now}
+
+});
+
+const Answer = models.Answer || model("Answer", AnswerSchema);
+
+export default Answer;
