17b70975c0b99bc751ee6d45a0721948ff5944b3
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-08 14:51:34 +0000

    Update Clerk webhook route and user creation logic

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -23,6 +23,7 @@ export async function createUser(userData: CreateUserParams) {
     connectToDatabase();
 
     const newUser = await User.create(userData);
+    console.log("newUser", newUser);
 
     return newUser;
   } catch (error) {
--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook/clerk"],
-  ignoredRoutes: ["/api/webhook/clerk"],
+  publicRoutes: ["/", "/api/webhooks(.*)",  "question/:id", "/tags", "/profile/:id", "/community"],
+  ignoredRoutes: ["/api/webhooks(.*)","/api/chatgpt"],
 });
 
 export const config = {
