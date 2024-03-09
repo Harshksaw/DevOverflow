b69433ab64f101f842f804664f94220617a80e1a
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-09 21:33:41 +0530

    Add new page component for editing questions and implement edit and delete actions

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -4,6 +4,8 @@ import RenderTag from "@/components/shared/RenderTag";
 import Metric from "@/components/shared/Metric";
 
 import { getFormattedNumber ,  getTimestamp } from "@/lib/utils";
+import { SignedIn } from "@clerk/nextjs";
+import EditDeleteAction from "../shared/EditDeleteAction";
 
 interface QuestionProps {
   _id: string;
@@ -33,6 +35,9 @@ const QuestionCard = ({
   createdAt,
   clerkId,
 }: QuestionProps) => {
+
+  const showActionButtons = clerkId && clerkId === author.clerkId;
+
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
@@ -48,6 +53,17 @@ const QuestionCard = ({
         </div>
 
         {/* If signed in add edit delete actions */}
+        <SignedIn>
+
+          {showActionButtons && (
+            <EditDeleteAction
+            type="Question" itemId = {JSON.stringify(_id)}
+            />
+
+          )
+
+          }
+        </SignedIn>
       </div>
 
       <div className="mt-3.5 flex flex-wrap gap-2">
