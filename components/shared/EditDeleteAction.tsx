ce7c1da35c57decfe2dbeba63f6967252947bb26
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-16 15:39:39 +0000

    Refactor EditDeleteAction component

--- components/shared/EditDeleteAction.tsx
+++ components/shared/EditDeleteAction.tsx
@@ -1,66 +1,60 @@
-"use client"
-import { deleteAnswer, deleteQuestion } from '@/lib/actions/question.action';
-import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-import { useRouter } from 'next/router';
-import React from 'react'
-interface Props{
-    type: string;
-    itemId: string;
+"use client";
 
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+
+import { deleteAnswer } from "@/lib/actions/answer.action";
+import { deleteQuestion } from "@/lib/actions/question.action";
+
+interface Props {
+  type: string;
+  itemId: string;
 }
-const EditDeleteAction = ({
+const EditDeleteAction = ({ type, itemId }: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
 
-
-    type,
-    itemId
-}: Props) => {
-    const pathname = usePathname();
-
-    const router = useRouter();
-
-
-
-    const handleEdit = () => {
-
-        router.push(`/question/edit/${JSON.parse(itemId)}`)
+  const handleEdit = () => {
+    if (type === "Question") {
+      router.push(`/question/edit/${JSON.parse(itemId)}`);
+    } else if (type === "Answer") {
+      router.push(`/edit-answer/${JSON.parse(itemId)}`);
     }
-    const handleDelete =async () => {
+  };
 
-        if(type == 'Question'){
-
-            await deleteQuestion({questionId: JSON.parse(itemId ), path: pathname})
-        }else if(type =='Answer'){
-            await deleteAnswer({answerId: JSON.parse(itemId), path: pathname})
-        }
-    
-        
+  const handleDelete = async () => {
+    if (type === "Question") {
+      await deleteQuestion({
+        questionId: JSON.parse(itemId),
+        path: pathname,
+        isQuestionPath: pathname === `/question/${JSON.parse(itemId)}`,
+      });
+    } else if (type === "Answer") {
+      await deleteAnswer({ answerId: JSON.parse(itemId), path: pathname });
     }
-
+  };
 
   return (
-    <div className='flex items-center justify-end max-sm:w-full  gap-3'>
-        {type === "Question" && (
-            <Image
-            src="/assets/icons/edit.svg"
-            alt="Edit"
-            width={14}
-            height={14}
-            className='cursor-pointer object-contain'
-            onClick={handleEdit}
-            />
-        )}
-         <Image
-            src="/assets/icons/delete.svg"
-            alt="Edit"
-            width={14}
-            height={14}
-            className='cursor-pointer object-contain'
-            onClick={handleDelete}
-            />
-      
-    </div>
-  )
-}
+    <div className="flex items-center justify-end gap-3 max-sm:w-full">
+      <Image
+        src="/assets/icons/edit.svg"
+        alt="Edit"
+        width={14}
+        height={14}
+        className="cursor-pointer"
+        onClick={handleEdit}
+      />
 
-export default EditDeleteAction
+      <Image
+        src="/assets/icons/trash.svg"
+        alt="Delete"
+        width={14}
+        height={14}
+        className="cursor-pointer"
+        onClick={handleDelete}
+      />
+    </div>
+  );
+};
+
+export default EditDeleteAction;
